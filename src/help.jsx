50aa4f3e1338eff2ffd12cf8a88068528dceb84f
vs-tries-to-code <vishnusree1805@gmail.com>
2026-06-16 20:59:36 +0530

    Help page jsx added [without footer]

---
+++ src/help.jsx
@@ -0,0 +1,48 @@
+const contacts = [
+    {
+        id : 1,
+        name : "Abel John Jose",
+        role : "Department Representative",
+        phoneNo : "+91 1234567890",
+        mail : "example@email.com"
+    },
+
+    {   
+        id : 2,
+        name : "Abel John Jose",
+        role : "Department Representative",
+        phoneNo : "+91 1234567890",
+        mail : "example@email.com"
+    },
+
+    {   
+        id : 3,
+        name : "Abel John Jose",
+        role : "Department Representative",
+        phoneNo : "+91 1234567890",
+        mail : "example@email.com"
+    },
+
+];
+
+function Help(){
+    return (
+        <main className ="help-page">
+        <h1>Help</h1>
+        <p>Questions, concerns or suggestions? Get in touch with us</p>
+        <div className = "contact-flex">
+            {
+                contacts.map((contact) => (
+                    <div key={contact.id} className="contact">
+                        <p>{contact.name}</p>
+                        <p>{contact.role}</p>
+                        <p>{contact.phoneNo}</p>
+                        <a href='{mailto:${contact.mail}'>{contact.mail}</a>
+                    </div>
+    ))}
+        </div>
+        </main>
+    )
+}
+
+export default Help;
